a32b607b4e6d50666d4bea145260522f1b89ddee
John Dave Decano <johndavedecano@gmail.com>
2021-05-19 04:10:25 +0800

    added callback

--- index.js
+++ index.js
@@ -3,15 +3,25 @@ const fs = require('fs')
 
 require('dotenv').config()
 
-httpProxy
+const PROXY_PORT = process.env.PROXY_PORT || 8003
+const TARGET_HOST = process.env.TARGET_HOST || 'localhost'
+const TARGET_PORT = process.env.TARGET_PORT || 8000
+const SSL_KEY_PATH = process.env.SSL_KEY_PATH || 'server.key'
+const SSL_CERT_PATH = process.env.SSL_CERT_PATH || 'server.cert'
+
+const server = httpProxy
   .createServer({
     target: {
-      host: process.env.TARGET_HOST || 'localhost',
-      port: process.env.TARGET_PORT || 8000,
+      host: TARGET_HOST,
+      port: TARGET_PORT,
     },
     ssl: {
-      key: fs.readFileSync('server.key', 'utf8'),
-      cert: fs.readFileSync('server.cert', 'utf8'),
+      key: fs.readFileSync(SSL_KEY_PATH, 'utf8'),
+      cert: fs.readFileSync(SSL_CERT_PATH, 'utf8'),
     },
   })
-  .listen(process.env.PROXY_PORT || 8003)
+  .listen(PROXY_PORT)
+
+server.once('start', () => {
+  console.log('Server started at port ' + PROXY_PORT)
+})
